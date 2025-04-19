19036f89107f5fa745a86899f7fa666ba1ff8ed0
AbhiCodes <chhettriabhishek9@gmail.com>
2025-04-19 16:15:24 -0400

    updating delete

--- src/app/api/[id]/route.js
+++ src/app/api/[id]/route.js
@@ -4,7 +4,7 @@ export async function DELETE(req, { params }) {
   const { id } = await params;
   try {
     await run();
-    User.findByIdAndDelete(id);
+    await User.findByIdAndDelete(id);
     return Response.json({ success: true });
   } catch (error) {
     return Response.json({ erro: "Failed to delete user" }, { status: 500 });
