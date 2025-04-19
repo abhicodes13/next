56e6cff77df12c1c117cd0d29fea84feeb65a5a1
AbhiCodes <chhettriabhishek9@gmail.com>
2025-04-19 15:58:36 -0400

    made [id] to handle delete

---
+++ src/app/api/[id]/route.js
@@ -0,0 +1,9 @@
+export async function DELETE(req, { params }) {
+  const { id } = await params;
+  try {
+    User.findByIdAndDelete(id);
+    return Response.json({ success: true });
+  } catch (error) {
+    return Response.json({ erro: "Failed to delete user" }, { status: 500 });
+  }
+}
