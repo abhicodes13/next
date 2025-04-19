63fbe9c471976b6d41eff1c4824a0ffdaed98aec
AbhiCodes <chhettriabhishek9@gmail.com>
2025-04-19 16:10:05 -0400

    delete [id]

--- src/app/api/[id]/route.js
+++ src/app/api/[id]/route.js
@@ -1,7 +1,9 @@
-import { User } from "../route";
+import { run, User } from "../route";
+
 export async function DELETE(req, { params }) {
   const { id } = await params;
   try {
+    await run();
     User.findByIdAndDelete(id);
     return Response.json({ success: true });
   } catch (error) {
--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-async function run() {
+export async function run() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
